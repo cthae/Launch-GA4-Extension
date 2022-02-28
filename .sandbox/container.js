c1c3482f13b799234ba71b5ce83f4d5d5c59ebfe
Bogdan Nazaruk <55302327+cthae@users.noreply.github.com>
2022-02-27 18:16:10 -0800

    GA4 settings DE, plus small fixes

---
+++ .sandbox/container.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "extensions": {},
+  "dataElements": {},
+  "rules": [],
+  "property": {
+    "settings": {
+      "id": "PR12345",
+      "domains": ["example.com"],
+      "undefinedVarsReturnEmpty": false
+    }
+  },
+  "company": {
+    "orgId": "ABCDEFGHIJKLMNOPQRSTUVWX@AdobeOrg"
+  },
+  "buildInfo": {
+    "turbineVersion": "27.2.0",
+    "turbineBuildDate": "2022-02-27T23:48:15.029Z",
+    "buildDate": "2022-02-27T23:48:15.029Z",
+    "environment": "development"
+  }
+}
